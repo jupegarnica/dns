67b4985ab39df466de9134d96bb59d23018f647b
Juan Garnica <juan@harbiz.io>
2024-11-09 17:43:20 +0100

    refactor: remove unused NotFound component and 404 URL handling logic

--- serve.tsx
+++ serve.tsx
@@ -5,14 +5,7 @@ import {
     serve,
 } from "https://deno.land/x/sift@0.6.0/mod.ts";
 
-import { join } from "https://deno.land/std@0.126.0/path/mod.ts";
 import { Dns, resolveDns } from "./resolveDns.jsx";
-
-const NotFound = () => (
-    <div>
-        <h1>Page not found</h1>
-    </div>
-);
 
 serve({
     "/:slug": async (_, connInfo, params) => {
@@ -20,17 +13,6 @@ serve({
         const domain = params?.slug ?? "garn.dev";
 
         return jsx(<Dns records={await resolveDns(domain)} domain={domain} />);
-    },
-
-    404: (req: Request) => {
-        let url = req.url;
-        if (url.endsWith("index.html")) {
-            url = url.split("index.html").join('garn.dev');
-        }
-        console.log(url);
-
-        const nextUrl = new URL(join(url, "./index.html"));
-        return Response.redirect(nextUrl, 302);
     },
 }, {
 
